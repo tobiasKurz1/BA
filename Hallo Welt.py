28fbf7a6989b1e7db5804debf2ddb8d7dc283fde
tobiasKurz1 <104297637+tobiasKurz1@users.noreply.github.com>
2022-04-25 19:48:25 +0200

    Create Hallo Welt.py

---
+++ Hallo Welt.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+Maximum = input("Zählen bis ..? ")
+
+
+for x in range(1, int(Maximum) + 1):
+    print("Zahl: %d" % x)
+
+print("Fertig mit Zählen!")
