bc53dc74e7d74051c2b70e7d17b45810bc4f8846
tobiasKurz1 <104297637+tobiasKurz1@users.noreply.github.com>
2022-06-23 16:04:32 +0200

    Neue Datei zum Einlesen der SPENVIS Dateien erstellt

---
+++ SPENVIS_einlesen.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 23 2022
+
+Einlesen einer CSV-Datei aus SPENVIS und Ausgabe bestimmter Daten
+
+genutzte Module: tabulate
+
+@author: Tobias Kurz
+"""
--- Werte_Einlesen.py
+++ Werte_Einlesen.py
@@ -15,12 +15,17 @@ import csv
 with open('test.csv')as werte:
     csv_reader_object = csv.reader(werte, delimiter=';')
     
+    
     zeilennummer = 0    #Zaehler für die Gesamtzeilenzahl
     counter = 0         #Zaehler für die Ausgegebenen Zeilen
+    EndeZeile = 0       #Bereichsende wird bereits definiert
+    
+    print("Welche Zeilen sollen ausgegeben werden?")
+    StartZeile = int(input("Von Zeile... (0 für gesamte Ausgabe)\n"))
+    if StartZeile != 0:
+        EndeZeile = int(input("Bis Zeile...\n"))
     
     
-    SucheZeile = int(input("Welche Zeile ausgeben? (für komplette Ausgabe 0 eingeben.) \n"))
-
     ergebnisse = [] #Hier werden Später die Zeilen abgelegt
     
     #Iteration der Zeilen durch den Reader, "row" immer die aktuell eingelesene Zeile
@@ -29,22 +34,28 @@ with open('test.csv')as werte:
         # Oberste Zeile -> Ueberschriften festlegen
         if zeilennummer == 0:
             beschriftung = ["Eintrag Nr."] + row # Einfügen einer Zaehlspalte in Spalte 1
+
         else:
-            if zeilennummer == SucheZeile or SucheZeile == 0:
+            if (zeilennummer >= StartZeile and zeilennummer <= EndeZeile) or (StartZeile == 0):
                 ergebnisse.append([zeilennummer]+row) #befuellen der Ergebnisliste und Aufzaehlen der Zeilen
                 counter += 1
         zeilennummer += 1
 
 
-#  schöne Tabellenform
-from tabulate import tabulate
-print("\n")
-print(tabulate(ergebnisse, headers=beschriftung))
+###### Ausgaben ######
 
 #  Meldung bei zu hoher Zeilenangabe
-if SucheZeile > (zeilennummer-1):
-    print("\nAusgewählte Zeile nicht vorhanden.")
+if (StartZeile > (zeilennummer-1)):
+    print("\nAusgewählter Bereich nicht vorhanden.")
+else: 
+    #  schöne Tabellenform
+    from tabulate import tabulate
+    print("\n")
+    print(tabulate(ergebnisse, headers=beschriftung))
     
+    if (EndeZeile > (zeilennummer-1)):
+        print("\nAusgewählter Bereich nicht vollständig vorhanden.")
+            
 #  Ausgabe verschiedener Infos über die Datenbank
 print(f'\nAnzahl ausgegebene Zeilen: {counter}/{zeilennummer-1}')
 print(f'Anzahl ausgegebener Einträge: {counter*(len(beschriftung)-1)}/{(zeilennummer-1)*(len(beschriftung)-1)}')
