1a5ca8c98cf08fc48ed0c6133163cf3ca8a340c6
tobiasKurz1 <104297637+tobiasKurz1@users.noreply.github.com>
2022-06-25 10:50:36 +0200

    kann nun erste 12 datensätze plotten

--- SPENVIS Daten/daten_plotten.py
+++ SPENVIS Daten/daten_plotten.py
@@ -32,6 +32,40 @@ def ende_block(ls):
     else:
         return (0)
 
+# Sammelt Daten aus den Bereichen in Arrays
+def daten_sammeln(grenze_u, grenze_o):
+    if counter >=grenze_u and counter <=grenze_o:
+        x_werte.append(float(row[0]))
+        y_werte1.append(float(row[1]))
+        y_werte2.append(float(row[2]))
+
+# Sammelt Achsenbeschriftungen aus dem Bereich über dem Datenblock in Arrays        
+def beschiftung_sammeln(anf_daten):
+    if counter >=(anf_daten-3) and counter <= (anf_daten-1):
+        achsen.extend((row[3], row[1]))
+        
+# Plottet aktuelle Achsen und Daten
+def plot_daten():  
+    fig, ax1 = plt.subplots()
+
+    color = 'tab:red'
+    ax1.set_xlabel(f'Block {blocknr}: {achsen[0]} [{achsen[1]}]') #Benennung x-Achse
+    ax1.set_ylabel(f'{achsen[2]} [{achsen[3]}]', color=color)
+    ax1.plot(x_werte, y_werte1, color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+    color = 'tab:blue'
+    ax2.set_ylabel(f'{achsen[4]} [{achsen[5]}]', color=color)  # we already handled the x-label with ax1
+    ax2.plot(x_werte, y_werte2, color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    plt.xscale('log')
+    plt.show()
+    
+
 
 #################################################################################
 ####
@@ -44,69 +78,48 @@ from tabulate import tabulate
 import csv
 import matplotlib.pyplot as plt
 
-
+##  Anfangsposition der Daten Blöcke 1-12 (Achsenbeschriftungen in den drei Zeilen davor)
+daten = [76, 181, 287, 392, 498, 603, 709, 814, 920, 1025, 1085, 1144]
+##  Anzahl Zeilen der Datenblöcke
+datenlaenge = [29, 30, 29, 30, 29, 30, 29, 30, 29, 30, 29, 30]
 counter = 1
-
+blocknr = 0
 ergebnisse = [] #Hier werden Später die Zeilen abgelegt
 x_werte = []
 y_werte1 = []
 y_werte2 = []
 achsen = []
 
-test = []
-
-def daten_sammeln(grenze_u, grenze_o):
-    if counter >=grenze_u and counter <=grenze_o:
-        x_werte.append(float(row[0]))
-        y_werte1.append(float(row[1]))
-        y_werte2.append(float(row[2]))
-        
-def beschiftung_sammeln(grenze_u, grenze_o):
-    if counter >=grenze_u and counter <=grenze_o:
-        achsen.extend((row[3], row[1]))
-        
-
 
 #CSV-Reader bekommt Datei, mit Anweisung um welche Abstandshalter es sich handelt
-with open('block_1.txt')as werte: # <-- Hier Dateinamen einstellen
+with open('Average_proton_and_electron_fluxes.txt')as werte: # <-- Hier Dateinamen einstellen
     csv_reader_object = csv.reader(werte, delimiter=',') # <-- Hier Abstandshalter einstellen
       
-        
-
-        
     #Iteration der Zeilen durch den Reader, "row" immer die aktuell eingelesene Zeile
-    #Indirekt können hierdurch bestimmte Zeilen ausgegeben werden
     for row in csv_reader_object:
         # Oberste Zeile -> Ueberschriften festlegen
         
+        print(counter)
         
-            ergebnisse.append(row) #befuellen der Ergebnisliste und Aufzaehlen der Zeilen
-            daten_sammeln(76, 104)
-            beschiftung_sammeln(73,75)
-            counter += 1
+        if anfang_block(row)==1:
+            blocknr += 1
+        
+        ergebnisse.append(row) #befuellen der Ergebnisliste und Aufzaehlen der Zeilen
+        daten_sammeln(daten[blocknr-1], daten[blocknr-1]+datenlaenge[blocknr-1]-1)
+        beschiftung_sammeln(daten[blocknr-1])
+                        
+        if ende_block(row)==1:
+            achsen = [s.replace("'","") for s in achsen]
+            plot_daten()
+            achsen.clear()
+            x_werte.clear()
+            y_werte1.clear()
+            y_werte2.clear()
+            
+        counter += 1
 
 print (tabulate(ergebnisse))
 
-achsen = [s.replace("'","") for s in achsen]
 
-######### Plot ##########
 
-fig, ax1 = plt.subplots()
-
-color = 'tab:red'
-ax1.set_xlabel(f'{achsen[0]} [{achsen[1]}]') #Benennung x-Achse
-ax1.set_ylabel(f'{achsen[2]} [{achsen[3]}]', color=color)
-ax1.plot(x_werte, y_werte1, color=color)
-ax1.tick_params(axis='y', labelcolor=color)
-
-ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-
-color = 'tab:blue'
-ax2.set_ylabel(f'{achsen[4]} [{achsen[5]}]', color=color)  # we already handled the x-label with ax1
-ax2.plot(x_werte, y_werte2, color=color)
-ax2.tick_params(axis='y', labelcolor=color)
-
-fig.tight_layout()  # otherwise the right y-label is slightly clipped
-plt.xscale('log')
-plt.show()
 
